dcfd75e8907f0b63ac5b063dc1faece28b361054
rnkazman <kazman@hawaii.edu>
2024-09-20 00:35:16 -1000

    Dictionary exercises

---
+++ Lists/Ex6.py
@@ -0,0 +1,9 @@
+ # Exercise 3.6: Create a dictionary and output it, and the Trip_miles element
+
+taxiTripInfo = {"Trip_id": "da7a62fce",
+                "Trip_seconds": 360,
+                "Trip_miles": 1.1,
+                "Fare": "$6.25"}
+
+print("taxiTripInfo:", taxiTripInfo)
+print("Trip miles:", taxiTripInfo["Trip_miles"])
