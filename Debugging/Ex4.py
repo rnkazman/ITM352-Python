26b56b58cf48b0305e999db5189ce0613528ed77
rnkazman <kazman@hawaii.edu>
2024-09-24 10:35:41 -1000

    Debugging module

---
+++ Debugging/Ex4.py
@@ -0,0 +1,7 @@
+def get_element(list, index):
+    return list[index]
+
+
+my_list = [1, 2, 3, 4, 5]
+print(get_element(my_list, 2))  
+print(get_element(my_list, 5))  
---
+++ Debugging/Ex5.py
@@ -0,0 +1,8 @@
+def fibonacci(list):
+    Fibo = list[0]
+    for val in list:
+        Fibo = Fibo + val
+    return Fibo
+
+my_list = [1, 2, 3, 4, 5]
+print(fibonacci(my_list))  
