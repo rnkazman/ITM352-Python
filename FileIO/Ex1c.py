5d87e3f9850256e524dc34f5f882c7bc75a6d8ee
Rick Kazman <kazman@hawaii.edu>
2024-10-09 10:14:14 -1000

    Update Ex1c.py

--- FileIO/Ex1c.py
+++ FileIO/Ex1c.py
@@ -1,7 +1,13 @@
 # Read a file of names.  Print them and print the number of names in the file. 
 
 with open("Names.txt") as my_file:
-    print(my_file.read())   
+    the_file = my_file.read() 
+    print(the_file) 
+
+print("Got the file\n")  
+splitup = the_file.split("\n")
+print(splitup)
+print("Length=", len(splitup))
 
 with open("Names.txt") as nameFile:
     count = 0
