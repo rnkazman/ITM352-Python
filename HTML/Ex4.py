33196644b9a4d07b84213188e116946ec233602a
rnkazman <kazman@hawaii.edu>
2024-10-27 16:23:16 -1000

    Update Ex4.py

--- HTML/Ex4.py
+++ HTML/Ex4.py
@@ -1,15 +1,22 @@
 # Extracting JSON data from the web example.  This is data on passenger vehicle licenses.
-
 import pandas as pd
 from sodapy import Socrata
 
-# The follwowing initializes the Socrata client:
+#pd.set_option("display.max_rows", None)
+
+# Initialize the Socrata client
 client = Socrata("data.cityofchicago.org", None)
 
-# Modify the following line, using the end of the URL, before the ".json"
+# Specify the JSON file we want to get from the client 
 results = client.get("rr23-ymwb", limit = 500)
 
-# The following puts the results in a dataframe & prints it:
+# Print the results as stored in the DataFrame 
 results_df = pd.DataFrame.from_records(results)
 print("Taxi API results:")
 print(results_df.head())
+
+vehicles_and_fuel_sources = results_df[["public_vehicle_number", "vehicle_fuel_source"]]
+print(f"Vehicles and fuel sources: \n{vehicles_and_fuel_sources}")
+
+vehicles_by_fuel_type = vehicles_and_fuel_sources.groupby("vehicle_fuel_source")
+print(vehicles_by_fuel_type.count())
