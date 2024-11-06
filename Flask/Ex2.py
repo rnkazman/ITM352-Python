1af5cd64ae4533d0835faf4524a9507e09543c1d
rnkazman <kazman@hawaii.edu>
2024-11-05 23:58:29 -1000

    Update Ex2.py

--- Flask/Ex2.py
+++ Flask/Ex2.py
@@ -12,7 +12,7 @@ def login():
     if request.method == 'POST':
         userid = request.form.get('username')
         userpass = request.form.get('password')
-        
+
         # Check the username and password.  If successful take the user to a success page.
         if USERS.get(userid) == userpass:
             return redirect(url_for('success', username=userid))
@@ -30,6 +30,6 @@ def success(username):
 USERS = {"port": "port123", 
          "kazman": "kazman123"}
 
-# Run the application
+# Run the application in debug mode.
 if __name__ == '__main__':
     app.run(debug=True)    
