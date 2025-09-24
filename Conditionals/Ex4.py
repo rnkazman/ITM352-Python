8dba751d72e51bb4bbb861db1243d0a6992c895e
rnkazman <kazman@hawaii.edu>
2025-09-24 00:05:54 -1000

    Conditionals lab

--- Conditionals/Ex4.py
+++ Conditionals/Ex4.py
@@ -1,23 +1,6 @@
-# Determine a movie price.  The rules are:
-# If someone is 65 or older, they pay $8.
-# If it is Tuesday, the price is $10.
-# If it is a matinee, the price is $5 for seniors and $8 otherwise
-# Print out the values of the variables and the price.  The price
-# should always be the lowest one that applies.
+year = int(input("Enter a year: "))
 
-age = 64
-day = "Tuesday"
-matinee = True
-price = 14
-
-if day == "Tuesday":
-    price = 10
-if age >= 65:
-    price =8
-if matinee == True:
-    if age >= 65:
-        price = 5
-    else:
-        price = 8
-        
-print("Price is: ", price)
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Leap year")
+else:
+    print("Not a leap year")
