12fd6c5956fcabfa64e605bb4d2418f5314ae346
rnkazman <kazman@hawaii.edu>
2025-11-04 22:34:30 -1000

    HTML updates

--- HTML/Ex2.py
+++ HTML/Ex2.py
@@ -14,12 +14,12 @@ print (f"Reading {url}...")
 try:
     tables = pd.read_html(url)
     int_rate_table = tables[0]
-    print(int_rate_table.columns)
+    #print(int_rate_table.columns)
     #print(int_rate_table.head())
 
+    print("\nTable of 1 month interest rates:")
     for index, row in int_rate_table.iterrows():
-       print(f"Index: {index}, 1 Mo: {row['1 Mo']}")
+       print(f"Index: {index}, Date: {row['Date']}, 1 Mo: {row['1 Mo']}")
    
-
 except Exception as e:
     print(f"An error occurred: {e}")
