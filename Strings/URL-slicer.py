de60fa9b6f940096792b005220754d8f91ba6831
rnkazman <kazman@hawaii.edu>
2026-02-02 21:40:57 -1000

    Update URL-slicer.py

--- Strings/URL-slicer.py
+++ Strings/URL-slicer.py
@@ -6,7 +6,7 @@ clean_url = url.replace("https://", "")
 # 2. Split by the dots
 parts = clean_url.split(".")
 
-# 3. Grab the middle piece (wikipedia)
+# 3. Grab the domain and the TLD 
 domain = parts[1]
 TLD = parts[2]
 
