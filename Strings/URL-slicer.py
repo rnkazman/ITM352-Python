773a2a35b617ee12957af6009f7a0e3ac66543fd
rnkazman <kazman@hawaii.edu>
2026-02-02 21:13:32 -1000

    Strings exercises

---
+++ Strings/URL-slicer.py
@@ -0,0 +1,14 @@
+url = input("Enter a URL: ")
+
+# 1. Strip off the protocol
+clean_url = url.replace("https://", "")
+
+# 2. Split by the dots
+parts = clean_url.split(".")
+
+# 3. Grab the middle piece (wikipedia)
+domain = parts[1]
+TLD = parts[2]
+
+print(f"The domain is: {domain}")
+print(f"The TLD is: {TLD}")
