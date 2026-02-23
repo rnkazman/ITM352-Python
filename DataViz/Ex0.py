aedcc6b006d84ea291d999cc8984a698778a80cc
rnkazman <kazman@hawaii.edu>
2026-02-22 21:46:34 -1000

    Update Ex0.py

--- DataViz/Ex0.py
+++ DataViz/Ex0.py
@@ -5,7 +5,7 @@ x_values = [1, 2, 3, 4, 5]
 y_values = [1, 3, 3, 3.5, 4]
 
 # Plot these values
-#plt.plot(x_values, y_values)
+plt.plot(x_values, y_values)
 plt.scatter(x_values, y_values)
 
 # Now add in a second set of values
